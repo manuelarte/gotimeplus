15dadf7080a2bce0dff2472d17597fb1f8267cba
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2024-12-05 23:32:34 +0100

    Improving the example

---
+++ examples/go.mod
@@ -0,0 +1,7 @@
+module examples
+
+go 1.23
+
+replace github.com/manuelarte/GoTime v0.0.0-20241202135225-37832ac5075a => ../
+
+require github.com/manuelarte/GoTime v0.0.0-20241202135225-37832ac5075a
--- examples/main.go
+++ examples/main.go
@@ -1,31 +1,33 @@
 package main
 
 import (
-	"GoTime/pkg/timeperiod"
 	"fmt"
+	"github.com/manuelarte/GoTime/pkg/timeperiod"
 	"time"
 )
 
+const timeFormat = "2006-January-02"
+
 func main() {
 	startTime := time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC)
-	endTime := startTime.Add(2 * time.Hour)
+	endTime := startTime.Add(31 * 24 * time.Hour)
 
 	timePeriod, err := timeperiod.NewTimePeriod(startTime, endTime)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("TimePeriod created:\tStartTime: %s\tEndTime: %s\n", timePeriod.GetStartTime(), timePeriod.GetEndTime())
+	fmt.Printf("TimePeriod1:\t%s\t - %s\n", timePeriod.GetStartTime().Format(timeFormat), timePeriod.GetEndTime().Format(timeFormat))
 
-	startTime2 := startTime.Add(1 * time.Hour)
-	endTime2 := endTime.Add(2 * time.Hour)
+	startTime2 := startTime.Add(14 * 24 * time.Hour)
+	endTime2 := endTime.Add(14 * 24 * time.Hour)
 	timePeriod2, err := timeperiod.NewTimePeriod(startTime2, endTime2)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("TimePeriod2 created:\tStartTime: %s\tEndTime: %s\n", timePeriod2.GetStartTime(), timePeriod2.GetEndTime())
+	fmt.Printf("TimePeriod2:\t%s\t - %s\n", timePeriod2.GetStartTime().Format(timeFormat), timePeriod2.GetEndTime().Format(timeFormat))
 
 	overlapPeriod, ok := timePeriod.Overlaps(timePeriod2)
 	if ok {
-		fmt.Printf("OverlapPeriod created:\tStartTime: %s\tEndTime: %s\n", overlapPeriod.GetStartTime(), overlapPeriod.GetEndTime())
+		fmt.Printf("OverlapPeriod:\t%s\t - %s\n", overlapPeriod.GetStartTime().Format(timeFormat), overlapPeriod.GetEndTime().Format(timeFormat))
 	}
 }
